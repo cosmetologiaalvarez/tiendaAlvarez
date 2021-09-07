c4a6d84cb6ebd3af4e8854fe27e84e619e68219c
TatiAbiAlvz <tatiadra@gmail.com>
2021-09-06 21:27:35 -0300

    entrega desafio clase 3

---
+++ src/components/navBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './navBar.css';
+import Grid from '@material-ui/core/Grid';
+
+const NavBar = () => {
+    return (
+        <Grid container className="navBarContent">
+            <Grid container className="navBarItem" item xs={4}>
+                <a href="#">Tienda</a>
+            </Grid>
+            <Grid container className="navBarItem" item xs={4}>
+                <a href="#">Us</a>
+            </Grid>
+            <Grid container className="navBarItem" item xs={4}>
+                <a href="#">Gallery</a>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default NavBar;
